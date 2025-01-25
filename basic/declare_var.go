3b9adcc560b717c14332be350dc93161983f5f8b
kashiwachen <oscar.chen@flywheel.jp>
2025-01-25 23:44:41 +0900

    style(gofmt): apply

--- basic/declare_var.go
+++ basic/declare_var.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func main() {
+func foo() {
 	var (
-		a int = 1
-		x, y int = 2, 3
-		z bool = true
+		a    int  = 1
+		x, y int  = 2, 3
+		z    bool = true
 	)
 	fmt.Printf("a: %v\n", a)
 	fmt.Printf("x, y: %v, %v\n", x, y)
